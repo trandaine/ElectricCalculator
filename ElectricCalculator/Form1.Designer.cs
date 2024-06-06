132b593de043e790c766a17f0679316b2734fd6c
Trần Quang Đại <tranquangdai03072004@outlook.com.vn>
2024-06-07 01:42:21 +0700

    Add project files.

---
+++ ElectricCalculator/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace ElectricCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            tbCustomerID = new TextBox();
+            tbCustomerName = new TextBox();
+            btnSubmit = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            lsvResult = new ListView();
+            txtboxLastIndex = new TextBox();
+            txtBoxCurrentIndex = new TextBox();
+            lblFinalCost = new Label();
+            label7 = new Label();
+            lblStatusSMS = new Label();
+            lblIDErrSms = new Label();
+            lblNameErrSMS = new Label();
+            lblLastIndexErrSMS = new Label();
+            lblCurrentIndexErrSms = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(53, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 20);
+            label2.TabIndex = 0;
+            label2.Text = "CustomerID";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 130);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Cusomter Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(584, 168);
+            label4.Name = "label4";
+            label4.Size = new Size(140, 20);
+            label4.TabIndex = 0;
+            label4.Text = "Consumption Index:";
+            label4.Click += label4_Click;
+            // 
+            // tbCustomerID
+            // 
+            tbCustomerID.Location = new Point(157, 60);
+            tbCustomerID.Name = "tbCustomerID";
+            tbCustomerID.Size = new Size(208, 26);
+            tbCustomerID.TabIndex = 1;
+            tbCustomerID.TextChanged += tbCustomerID_TextChanged;
+            // 
+            // tbCustomerName
+            // 
+            tbCustomerName.Location = new Point(157, 127);
+            tbCustomerName.Name = "tbCustomerName";
+            tbCustomerName.Size = new Size(208, 26);
+            tbCustomerName.TabIndex = 1;
+            tbCustomerName.TextChanged += tbCustomerName_TextChanged;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(591, 227);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(90, 28);
+            btnSubmit.TabIndex = 2;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(584, 63);
+            label5.Name = "label5";
+            label5.Size = new Size(75, 20);
+            label5.TabIndex = 0;
+            label5.Text = "Last Index";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(584, 113);
+            label6.Name = "label6";
+            label6.Size = new Size(97, 20);
+            label6.TabIndex = 0;
+            label6.Text = "Current Index";
+            label6.Click += label4_Click;
+            // 
+            // lsvResult
+            // 
+            lsvResult.Location = new Point(24, 281);
+            lsvResult.Name = "lsvResult";
+            lsvResult.Size = new Size(687, 320);
+            lsvResult.TabIndex = 3;
+            lsvResult.UseCompatibleStateImageBehavior = false;
+            lsvResult.View = View.Details;
+            // 
+            // txtboxLastIndex
+            // 
+            txtboxLastIndex.Location = new Point(730, 60);
+            txtboxLastIndex.Name = "txtboxLastIndex";
+            txtboxLastIndex.Size = new Size(208, 26);
+            txtboxLastIndex.TabIndex = 1;
+            txtboxLastIndex.TextChanged += textBox1_TextChanged;
+            // 
+            // txtBoxCurrentIndex
+            // 
+            txtBoxCurrentIndex.Location = new Point(730, 110);
+            txtBoxCurrentIndex.Name = "txtBoxCurrentIndex";
+            txtBoxCurrentIndex.Size = new Size(208, 26);
+            txtBoxCurrentIndex.TabIndex = 1;
+            txtBoxCurrentIndex.TextChanged += txtBoxCurrentIndex_TextChanged;
+            // 
+            // lblFinalCost
+            // 
+            lblFinalCost.AutoSize = true;
+            lblFinalCost.Location = new Point(724, 168);
+            lblFinalCost.Name = "lblFinalCost";
+            lblFinalCost.Size = new Size(143, 20);
+            lblFinalCost.TabIndex = 4;
+            lblFinalCost.Text = "(Result appear here)";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(24, 232);
+            label7.Name = "label7";
+            label7.Size = new Size(52, 20);
+            label7.TabIndex = 5;
+            label7.Text = "Status:";
+            // 
+            // lblStatusSMS
+            // 
+            lblStatusSMS.AutoSize = true;
+            lblStatusSMS.Location = new Point(88, 232);
+            lblStatusSMS.Name = "lblStatusSMS";
+            lblStatusSMS.Size = new Size(0, 20);
+            lblStatusSMS.TabIndex = 5;
+            // 
+            // lblIDErrSms
+            // 
+            lblIDErrSms.AutoSize = true;
+            lblIDErrSms.Location = new Point(157, 97);
+            lblIDErrSms.Name = "lblIDErrSms";
+            lblIDErrSms.Size = new Size(0, 20);
+            lblIDErrSms.TabIndex = 0;
+            lblIDErrSms.Click += label4_Click;
+            // 
+            // lblNameErrSMS
+            // 
+            lblNameErrSMS.AutoSize = true;
+            lblNameErrSMS.Location = new Point(157, 168);
+            lblNameErrSMS.Name = "lblNameErrSMS";
+            lblNameErrSMS.Size = new Size(0, 20);
+            lblNameErrSMS.TabIndex = 0;
+            lblNameErrSMS.Click += label4_Click;
+            // 
+            // lblLastIndexErrSMS
+            // 
+            lblLastIndexErrSMS.AutoSize = true;
+            lblLastIndexErrSMS.Location = new Point(963, 63);
+            lblLastIndexErrSMS.Name = "lblLastIndexErrSMS";
+            lblLastIndexErrSMS.Size = new Size(0, 20);
+            lblLastIndexErrSMS.TabIndex = 0;
+            lblLastIndexErrSMS.Click += label4_Click;
+            // 
+            // lblCurrentIndexErrSms
+            // 
+            lblCurrentIndexErrSms.AutoSize = true;
+            lblCurrentIndexErrSms.Location = new Point(963, 113);
+            lblCurrentIndexErrSms.Name = "lblCurrentIndexErrSms";
+            lblCurrentIndexErrSms.Size = new Size(0, 20);
+            lblCurrentIndexErrSms.TabIndex = 0;
+            lblCurrentIndexErrSms.Click += label4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1112, 645);
+            Controls.Add(lblStatusSMS);
+            Controls.Add(label7);
+            Controls.Add(lblFinalCost);
+            Controls.Add(lsvResult);
+            Controls.Add(btnSubmit);
+            Controls.Add(tbCustomerName);
+            Controls.Add(txtBoxCurrentIndex);
+            Controls.Add(txtboxLastIndex);
+            Controls.Add(tbCustomerID);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(lblNameErrSMS);
+            Controls.Add(lblCurrentIndexErrSms);
+            Controls.Add(lblLastIndexErrSMS);
+            Controls.Add(lblIDErrSms);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Name = "Form1";
+            Text = "Electric Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+#endregion
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox tbCustomerID;
+        private TextBox tbCustomerName;
+        private Button btnSubmit;
+        private Label label5;
+        private Label label6;
+        private ListView lsvResult;
+        private TextBox txtboxLastIndex;
+        private TextBox txtBoxCurrentIndex;
+        private Label lblFinalCost;
+        private Label label7;
+        private Label lblStatusSMS;
+        private Label lblIDErrSms;
+        private Label lblNameErrSMS;
+        private Label lblLastIndexErrSMS;
+        private Label lblCurrentIndexErrSms;
+    }
+}
---
+++ ElectricCalculator/Form1.cs
@@ -0,0 +1,177 @@
+using System.Drawing.Text;
+using System.Text.RegularExpressions;
+
+namespace ElectricCalculator
+{
+    public partial class Form1 : Form
+    {
+        string namePattern = @"[A-Za-z/s]$";      // Regular expression for name
+        string idPattern = @"[A-Za-z0-9]$";             // Regular expression for ID
+        string numberPattern = @"[0-9]$";       // Regular expression for number
+        private readonly float Pricelevel1_From0to50 = 1806F;
+        private readonly float Pricelevel2_From51to100 = 1866F; 
+        private readonly float Pricelevel3_From101to200 = 2167F;
+        private readonly float Pricelevel4_From201to300 = 2729F;
+        private readonly float Pricelevel5_From301to400 = 3050F;
+        private readonly float Pricelevel6_From401 = 3151F;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lsvResult.Columns.Add("Customer ID", 100);
+            lsvResult.Columns.Add("Customer Name", 100);
+            lsvResult.Columns.Add("Last Index", 100);
+            lsvResult.Columns.Add("Current Index", 100);
+            lsvResult.Columns.Add("Total Cost", 100);
+        }
+        private float calculateElectricPrice_v2(float consumptionIndex)
+        {
+            float price = 0;
+            int[]arrayindex = new int[6] {0,51,101, 201, 301, 401 };
+            float[]arrayPrice = new float[6] { 1806, 1866, 2167, 2729, 3050, 3151 };
+            for(int i = arrayindex.Length -1; i>=0; i--)
+            {
+                
+                float subIndex = consumptionIndex - arrayindex[i];
+                if(subIndex > 0)
+                {
+                    price += subIndex * arrayPrice[i];      // calculate the price
+                    //consumptionIndex -= subIndex;       // reduce the consumption index
+                }
+                else
+                {
+                    subIndex = arrayindex[i] - 1;
+                }
+            }
+            return price;
+        }
+        private float calculateElectricPrice(float index)
+        {
+            float total = 0;
+            if (index <= 50)
+            {
+                total = index * Pricelevel1_From0to50;
+            }
+            else if (index >= 51 && index <= 100)           // 51 - 100
+            {
+                total = (50 * Pricelevel1_From0to50) 
+                    + (index - 50) 
+                    * Pricelevel2_From51to100;
+            }
+            else if (index >= 101 && index <= 200)          // 101 - 200
+            {
+                total = (50 * Pricelevel1_From0to50) 
+                    + (50 * Pricelevel2_From51to100) 
+                    + (index - 100) 
+                    * Pricelevel3_From101to200;
+            }
+            else if (index >= 201 && index <= 300)          // 201 - 300
+            {
+                total = (50 * Pricelevel1_From0to50) 
+                    + (50 * Pricelevel2_From51to100) 
+                    + (100 * Pricelevel3_From101to200)
+                    + (index - 200) 
+                    * Pricelevel4_From201to300;
+            }
+            else if (index >= 301 && index <= 400)
+            {
+                total = (50 * Pricelevel1_From0to50) 
+                    + (50 * Pricelevel2_From51to100) 
+                    + (100 * Pricelevel3_From101to200) 
+                    + (100 * Pricelevel4_From201to300) 
+                    + (index - 300) 
+                    * Pricelevel5_From301to400;
+            }
+            else if (index >= 401)
+            {
+                total = (50 * Pricelevel1_From0to50) 
+                    + (50 * Pricelevel2_From51to100) 
+                    + (100 * Pricelevel3_From101to200) 
+                    + (100 * Pricelevel4_From201to300) 
+                    + (100 * Pricelevel5_From301to400) 
+                    + (index - 400) 
+                    * Pricelevel6_From401;
+            }
+            return total;
+        }
+        
+        private void tbCustomerID_TextChanged(object sender, EventArgs e)
+        {
+            Regex idRegex = new Regex(idPattern);
+            lblIDErrSms.Text = string.Empty;
+            //try {
+            //    var input = tbCustomerID.Text;
+            //    if(string.IsNullOrEmpty(input)) lblIDErrSms.Text = "Nho71 nha6p5 nha chan noi";
+            //    if (!idRegex.IsMatch(input))
+            //    {
+            //        lblIDErrSms.Text = "Invalid ID";
+            //        lblIDErrSms.ForeColor = Color.Red;
+            //    }
+
+            //} 
+            //catch (Exception ex)
+            //{ }
+            if (!idRegex.IsMatch(tbCustomerID.Text))
+            {
+                lblIDErrSms.Text = "Invalid ID";
+                lblIDErrSms.ForeColor = Color.Red;
+            }
+        }
+
+        private void tbCustomerName_TextChanged(object sender, EventArgs e)
+        {
+            Regex nameRegex = new Regex(namePattern);
+            lblNameErrSMS.Text = string.Empty;
+            if (!nameRegex.IsMatch(tbCustomerName.Text))
+            {
+                lblNameErrSMS.Text = "Invalid Name";
+                lblNameErrSMS.ForeColor = Color.Red;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            float total = this.calculateElectricPrice_v2(float.Parse(txtBoxCurrentIndex.Text) - float.Parse(txtboxLastIndex.Text));
+            lblStatusSMS.Text = "Success";
+            lblStatusSMS.ForeColor = Color.Green;
+            ListViewItem items = new ListViewItem(new string[]
+            {
+                this.tbCustomerID.Text,
+                this.tbCustomerName.Text,
+                this.txtboxLastIndex.Text,
+                this.txtBoxCurrentIndex.Text,
+                $"{total: #,###}VND",
+            });
+            this.lsvResult.Items.Add(items);
+            this.lsvResult.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            this.lsvResult.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Regex lstLastIndexRegex = new Regex(numberPattern);
+            lblLastIndexErrSMS.Text = string.Empty;
+            if (!lstLastIndexRegex.IsMatch(txtboxLastIndex.Text))
+            {
+                lblLastIndexErrSMS.Text = "Invalid Number";
+                lblLastIndexErrSMS.ForeColor = Color.Red;
+            }
+        }
+
+        private void txtBoxCurrentIndex_TextChanged(object sender, EventArgs e)
+        {
+            Regex lstCurrentIndexRegex = new Regex(numberPattern);
+            lblCurrentIndexErrSms.Text = string.Empty;
+            if (!lstCurrentIndexRegex.IsMatch(txtBoxCurrentIndex.Text))
+            {
+                lblCurrentIndexErrSms.Text = "Invalid Number";
+                lblCurrentIndexErrSms.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblFinalCost.Text = $"{int.Parse(txtBoxCurrentIndex.Text) - int.Parse(txtboxLastIndex.Text)}";
+            }
+        }
+    }   
+}
